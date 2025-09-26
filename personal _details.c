5ed3496bd20850aeee0bff772152664295c7942d
kelvin4-netizen <mucherukelvin4@gmail.com>
2025-09-26 20:42:45 +0300

    Add files via upload

---
+++ personal _details.c
@@ -0,0 +1,37 @@
+// simple c program to display my name
+/*
+Name:Kelvin
+Reg no :CT101/G/26445/25
+Descripions: A program that prompts the user to enter their personal  details 
+*/
+#include<stdio.h>
+int main(){
+
+   int height;
+    printf("enter your height \n");
+    scanf("%d",& height);
+    printf("your height is %d \n",height);
+    
+   int age;
+    printf("enter your age \n");
+    scanf("%d",&age);
+    printf("your age is %d \n",age);
+    
+   int balance;
+    printf("enter your bank balance \n");
+    scanf("%d",& balance);
+    printf("your bank balance is Ksh . %d \n",balance);
+    
+   int weight;
+    printf("enter your weight \n");
+    scanf("%d",& weight);
+    printf("your weight is %d \n", weight);
+    
+   int number;
+    printf("enter your phone number\n");
+    scanf("%d",&number);
+    printf("your phone number is %d \n",number);
+    
+   
+   return 0;
+}
