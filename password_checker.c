afe51cfbfd9ed6020f5fd9506366c162fb8af063
kelvin4-netizen <mucherukelvin4@gmail.com>
2025-10-09 21:08:11 +0300

    Add files via upload

---
+++ password_checker.c
@@ -0,0 +1,29 @@
+/*
+Name: Mucheru Kelvin Maina 
+Reg no: CT101/G/26445/25
+Description: Password check system
+*/
+
+
+#include<stdio.h>
+
+int main(){
+int password;
+
+do{
+
+printf("\nEnter your password.\n");
+scanf("%d",& password);
+
+if(password==1234){
+break;
+}
+printf("\nIncorrect password! Please try again. \n");
+}
+while (1);
+printf(" \nAccess Granted. \n");
+printf("Welcome user .");
+
+	return 0;
+}
+		
